11b4b6d7c69c01438a601de46416fd735f6cd76f
jmangata <jemalmangata27@gmail.com>
2025-12-04 14:47:04 -0400

    creation des modèles et des  migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Cart;
+use App\Models\Order;
 use App\Enums\UserRole;
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
